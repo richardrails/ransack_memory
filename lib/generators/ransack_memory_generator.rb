7bbc58533a360d92f3fe9e41f647235e24ca6d76
Richard Lapiš <richard.lapis@railsformers.com>
2018-03-07 08:01:39 +0100

    generator

--- lib/generators/ransack_memory_generator.rb
+++ lib/generators/ransack_memory_generator.rb
@@ -4,6 +4,7 @@ if defined?(Rails)
   class RansackMemoryGenerator < Rails::Generators::Base
     source_root(File.expand_path(File.dirname(__FILE__) + "/../templates/ransack_memory"))
     def copy_initializer
+
       copy_file 'ransack_memory_template.rb', 'config/initializers/ransack_memory.rb'
     end
   end
---
+++ lib/templates/ransack_memory/ransack_memory_template.rb
@@ -0,0 +1,3 @@
+RansackMemory.config = {
+  param: :q
+}
