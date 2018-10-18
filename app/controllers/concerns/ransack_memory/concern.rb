be5f8f3195dd6df4acfd3ef16700095151b16646
Richard Lapiš <richard.lapis@railsformers.com>
2018-10-18 09:31:00 +0200

    code cleanup

--- app/controllers/concerns/ransack_memory/concern.rb
+++ app/controllers/concerns/ransack_memory/concern.rb
@@ -3,54 +3,42 @@ module RansackMemory
     extend ActiveSupport::Concern
 
     def save_and_load_filters
+      
+      session_key_base = "#{controller_name}_#{action_name}_#{request.xhr?}"
 
       # cancel filter if button pressed
-      if params[:cancel_filter].present? && params[:cancel_filter] == "true"
-        @selected_filter = nil
-        session["#{controller_name}_#{action_name}_#{request.xhr?}"] = nil
-        session["#{controller_name}_#{action_name}_#{request.xhr?}_page"] = nil
+      if params[:cancel_filter] == "true"
+        session["#{session_key_base}"] = nil
+        session["#{session_key_base}_page"] = nil
+        session["#{session_key_base}_per_page"] = nil
       end
 
-      if user_signed_in?
-        # search term saving
-        session["#{controller_name}_#{action_name}_#{request.xhr?}"] = params[::RansackMemory::Core.config[:param]] if params[::RansackMemory::Core.config[:param]].present?
+      # search term saving
+      session["#{session_key_base}"] = params[::RansackMemory::Core.config[:param]] if params[::RansackMemory::Core.config[:param]].present?
 
-        # page number saving
-        session["#{controller_name}_#{action_name}_#{request.xhr?}_page"] = params[:page] if params[:page].present?
+      # page number saving
+      session["#{session_key_base}_page"] = params[:page] if params[:page].present?
 
-        # per page saving
-        session["#{controller_name}_#{action_name}_#{request.xhr?}_per_page"] = params[:per_page] if params[:per_page].present?
+      # per page saving
+      session["#{session_key_base}_per_page"] = params[:per_page] if params[:per_page].present?
+
+      # search term load
+      params[::RansackMemory::Core.config[:param]] = session["#{session_key_base}"].presence
+
+      # page number load
+      params[:page] = session["#{session_key_base}_page"].presence
+
+      # per page load
+      params[:per_page] = session["#{session_key_base}_per_page"].presence
+
+      # set page number to 1 if filter has changed
+      if (params[::RansackMemory::Core.config[:param]].present? && session[:last_q_params] != params[::RansackMemory::Core.config[:param]]) || (params[:cancel_filter].present? && session["#{session_key_base}_page"] != params[:page])
+        params[:page] = nil
+        session["#{session_key_base}_page"] = nil
       end
 
-      if user_signed_in?
-        # search term load
-        if session["#{controller_name}_#{action_name}_#{request.xhr?}"]
-          params[::RansackMemory::Core.config[:param]] = session["#{controller_name}_#{action_name}_#{request.xhr?}"]
-        else
-          params[::RansackMemory::Core.config[:param]] = nil
-        end
-
-        # page number load
-        if session["#{controller_name}_#{action_name}_#{request.xhr?}_page"]
-          params[:page] = session["#{controller_name}_#{action_name}_#{request.xhr?}_page"]
-        else
-          params[:page] = nil
-        end
-
-        # set page number to 1 if filter has changed
-        if (params[::RansackMemory::Core.config[:param]] && session[:last_q_params] != params[::RansackMemory::Core.config[:param]]) || (params[:cancel_filter].present? && session["#{controller_name}_#{action_name}_#{request.xhr?}_page"] != params[:page])
-          params[:page] = nil
-          session["#{controller_name}_#{action_name}_#{request.xhr?}_page"] = nil
-        end
-        session[:last_q_params] = params[::RansackMemory::Core.config[:param]]
-
-        # per page load
-        if session["#{controller_name}_#{action_name}_#{request.xhr?}_per_page"]
-          params[:per_page] = session["#{controller_name}_#{action_name}_#{request.xhr?}_per_page"]
-        else
-          params[:per_page] = nil
-        end
-      end
+      session[:last_q_params] = params[::RansackMemory::Core.config[:param]]
+      # session[:last_page] = params[:page]
     end
   end
 end
